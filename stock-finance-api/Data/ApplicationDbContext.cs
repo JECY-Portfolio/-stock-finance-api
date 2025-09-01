6c0fb4347fe460e72289393c90e78c1e73154ea3
Geoffrey Jecinta <jecintageoffrey@yahoo.com>
2025-09-01 19:14:43 +0100

    Install and configure Entity Framework Core, create ApplicationDbContext and apply Initial Migration

---
+++ stock-finance-api/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using stock_finance_api.Models;
+
+namespace stock_finance_api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
--- stock-finance-api/Models/Comment.cs
+++ stock-finance-api/Models/Comment.cs
@@ -1,6 +1,12 @@
-﻿namespace stock_finance_api.Models
+﻿namespace stock_finance_api.Models;
+
+public class Comment
 {
-    public class Comment
-    {
-    }
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+    public int? StockId { get; set; }
+    public Stock? Stock { get; set; }
 }

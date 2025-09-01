e11bf8e74f6621b474bd76ddd5a448741151319a
Geoffrey Jecinta <jecintageoffrey@yahoo.com>
2025-09-01 20:46:12 +0100

    Implement GET endpoint for Stocks API

---
+++ stock-finance-api/Controllers/StocksController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using stock_finance_api.Data;
+
+namespace stock_finance_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StocksController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public StocksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetALl()
+        {
+            var stocks = _context.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if(stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+    }
+}

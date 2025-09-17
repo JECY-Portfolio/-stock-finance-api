0ca2c16555fc5423f9cd6c11cc5008c016daff59
Geoffrey Onyedikachi <jecintageoffrey@yahoo.com>
2025-09-17 15:34:22 +0100

    Added Portfolio endpoints: Get, Create, and Delete

---
+++ stock-finance-api/Interfaces/IPortfolioRepository.cs
@@ -0,0 +1,12 @@
+﻿using stock_finance_api.Models;
+using System.Threading.Tasks;
+
+namespace stock_finance_api.Interfaces
+{
+    public interface IPortfolioRepository
+    {
+        Task<List<Stock>> GetUserPortfolio(AppUser user);
+        Task<Portfolio> CreateAsync(Portfolio portfolio);
+        Task<Portfolio> DeletePortfolio(AppUser user, string symbol);
+    }
+}
